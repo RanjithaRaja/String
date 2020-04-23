dd7631c0431f0f000239656f82d21c512ac1fbbe
Ranjitha Raja <ranjitharaja5@gmail.com>
2020-04-23 22:51:59 +0530

    Update Reverse-words.js

--- Reverse-words.js
+++ Reverse-words.js
@@ -1,18 +1,35 @@
-Given an input string, reverse the string word by word.
+// Given an input string, reverse the string word by word.
 
-Example 1:
+// Example 1:
 
-Input: "the sky is blue"
-Output: "blue is sky the"
+// Input: "the sky is blue"
+// Output: "blue is sky the"
 
-Example 2:
+// Example 2:
 
-Input: "  hello world!  "
-Output: "world! hello"
-Explanation: Your reversed string should not contain leading or trailing spaces.
+// Input: "  hello world!  "
+// Output: "world! hello"
+// Explanation: Your reversed string should not contain leading or trailing spaces.
 
-Example 3:
+// Example 3:
 
-Input: "a good   example"
-Output: "example good a"
-Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+// Input: "a good   example"
+// Output: "example good a"
+// Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+var reverseWords = function (s) {
+  let str = s.trim().split(" ")
+  let result = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i] != "") {
+      result += str[i] + " "
+    }
+  }
+
+  return result.trim()
+};
+
+console.log(reverseWords("  hello world!  "));
+console.log(reverseWords("the sky is blue"))
+console.log(reverseWords("a good   example"))
